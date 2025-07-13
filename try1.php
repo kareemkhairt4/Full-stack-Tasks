48c4a8f523f6a64a5b610d385e14339006033361
kareemkhairt4 <158579791+kareemkhairt4@users.noreply.github.com>
2025-07-14 02:59:07 +0300

    Add files via upload

---
+++ try1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="Tasks/DAY1/css/bootstrap.css" rel="stylesheet">
+</head>
+
+<body style="background-color: #d0f0c0;">
+    <div class="container">
+        <div class="row mt-4 p-5 justify-content-center">
+            <div class="col-lg-4">
+
+                <form class="w-100 bg-light p-5">
+                    <h2 class="text-center mn-4">User Information </h2>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail2" aria-describedby="emailHelp">
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Age</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">City</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-sw w-100 text-center mt-3">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="Tasks/DAY1/js/bootstrap.bundle.js"></script>
+</body>
+
+</html>
